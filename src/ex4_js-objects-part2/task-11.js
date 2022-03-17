808f3980f3db47d025f199b501c1abbc4768c9e9
kt-postnikova <kt-postnikova@yandex.ru>
2022-03-17 14:56:58 +0300

    fix: пустой вызов console.log

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,6 +1,10 @@
 module.exports = function countLogs(string) {
-  const newString = new Set(string);
-  for (let i = 0; i < newString.size; i += 1) {
-    console.log();
-  }
+  const lettersObj = {};
+  string.split('').forEach((letter) => {
+    lettersObj[letter] = (lettersObj[letter] || 0) + 1;
+  });
+  const lettersArr = Object.entries(lettersObj);
+  lettersArr.forEach((letter) => {
+    console.log(letter.join(': '));
+  });
 };
