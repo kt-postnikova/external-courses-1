6b2bb07154aab66453761deb7684bb95b3980502
kt-postnikova <kt-postnikova@yandex.ru>
2022-04-25 12:15:22 +0300

    Slider is done

---
+++ src/ex11_js_dom/script.js
@@ -0,0 +1,44 @@
+const leftButton = document.querySelector('.slider__button_type_left');
+const rightButton = document.querySelector('.slider__button_type_right');
+const slider = document.querySelector('.slider');
+const sliderBlock = slider.querySelectorAll('.slider__block');
+
+const slides = [
+  './assets/russia.jpg',
+  './assets/britain.jpg',
+  './assets/france.jpg',
+  './assets/germany.jpg',
+  './assets/states.jpg',
+]
+
+let index = 0;
+
+function showSlide(currentSlide) {
+  if (currentSlide > (slides.length - 1)) {
+    index = 0;
+  }
+  if (currentSlide < 0) {
+    index = slides.length - 1;
+  }
+
+  for (let slide of sliderBlock) {
+    slide.style.display = "none";
+  }
+
+  sliderBlock[index].style.display = "block";
+}
+
+showSlide(index)
+
+function nextSlide() {
+  showSlide(index += 1);
+}
+
+function previousSlide() {
+  showSlide(index -= 1)
+}
+
+
+rightButton.addEventListener('click', nextSlide);
+
+leftButton.addEventListener('click', previousSlide);
